73778f6a6bfa0deca21c5327a3319bd7671eb59a
God <sanyaatanasov@gmail.com>
2024-01-23 17:01:56 +0200

    funy

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print((3+2 > 3) == True)
+from datetime import datetime as dt
+import os
+
+def get_days_from_today(date):
+    date= dt.date(date)
+    today=dt.date(dt.today())
+    print(f'Дата яку ми ввели:\n{date}\n')
+    print(f'Поточна дата:\n{today}\n')
+    print(f'Різниця кількістю днів між заданою датою і поточною датою:\n{today-date}\n')
+    
+
+
+def start_main_program():
+    try:
+
+        input_time=dt(year=2023, month=1, day=6)
+        #input_time = dt(year=int(input("Year= ")), month=int(input("Month= ")), day=int(input("Day= ")))
+        get_days_from_today(input_time)
+        
+
+    except Exception as eror:
+        os.system('cls')
+        print(f"Error: {eror}")
+        start_main_program()
+
+
+start_main_program()
+
+
+    
